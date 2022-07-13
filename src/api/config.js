8fa9411bff31c67c8bce97e272ad186d2a6af55d
Prakhar Kapoor <kapoorprakhar99@gmail.com>
2022-07-13 11:57:35 +0530

    added api server url

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: "http://localhost:9001/api/v1",
+    baseURL: "https://carer-web-app-backend-server.herokuapp.com/api/v1",
 })
 
 export default instance
